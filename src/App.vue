6260e0063232274324fad03996d313a32737ec06
camizalcman <camizalcman@gmail.com>
2025-09-01 23:22:32 -0300

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+  const number = 5
+
+  function sum(a,b){
+    return a+b;
+  }
+</script>
+
+<template>
+  {{ sum(5,6) }}
+</template>
+
+<style scoped>
+
+  .text-red{
+    color:red
+  }
+</style>
