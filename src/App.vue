78f571b250b59197149dc0e495105317c55037dd
CamilaZalcman <camizalcman@gmail.com>
2025-09-04 12:00:44 -0300

    creacion form gastos

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <script>
   import Ingreso from './components/FormIngresos.vue'
+  import Gasto from './components/FormGastos.vue'
+
 
   export default {
       name: 'App',
       components: {
-        Ingreso //Lo registramos para poder usarlo en el template
+        Ingreso, //Lo registramos para poder usarlo en el template
+        Gasto
     },
 
     data() {
       return {
-        ingresos: [] //Para guardar todos los ingresos
+        ingresos: [], //Para guardar todos los ingresos
+        gastos: []
       }
     },
 
@@ -17,6 +21,11 @@
       agregarIngreso(nuevoIngreso) {
         // Agregamos el ingreso que viene del hijo al array de ingresos
         this.ingresos.push(nuevoIngreso)
+      },
+
+      agregarGasto(nuevoGasto) {
+        // Agregamos el gasto que viene del hijo al array de gastos
+        this.gastos.push(nuevoGasto)
       }
     }
   }
@@ -24,19 +33,38 @@
 </script>
 
 <template>
-  <div>
-    <h1>Gestor de Ingresos</h1>
-    <Ingreso @agregar-ingreso="agregarIngreso"/> <!-- @agregar-ingreso escucha el evento que el hijo emite con $emit y agregarIngreso es el método del padre que se ejecuta (definido acá) cuando ocurre ese evento -->
-    <h2>Lista de ingresos:</h2>
-    <ul>
-      <li v-for="ingreso in ingresos"><!-- v-for recorre el array-->
-        {{ ingreso.monto }} - {{ ingreso.categoria }}
-      </li>
-    </ul>
+  <div class="contenedor df">
+    <div>
+      <h1>Ingresos</h1>
+      <Ingreso @agregar-ingreso="agregarIngreso"/> <!-- Aca estoy agregando el componente Ingreso. @agregar-ingreso escucha el evento que el hijo emite con $emit y agregarIngreso es el método del padre que se ejecuta (definido acá) cuando ocurre ese evento -->
+      <h2>Lista de ingresos:</h2>
+      <ul>
+        <li v-for="ingreso in ingresos"><!-- v-for recorre el array-->
+          {{ ingreso.monto }} - {{ ingreso.categoria }}
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <h1>Gastos</h1>
+      <Gasto @agregar-gasto="agregarGasto"/> <!-- @agregar-gasto escucha el evento que el hijo emite con $emit y agregarGasto es el método del padre que se ejecuta (definido acá) cuando ocurre ese evento -->
+      <h2>Lista de gastos:</h2>
+      <ul>
+        <li v-for="gasto in gastos"><!-- v-for recorre el array-->
+          {{ gasto.monto }} - {{ gasto.categoria }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.df{
+  display: flex;
+}
+.contenedor{
+  gap:2em
+}
 
 
 </style>
--- src/components/FormGastos.vue
+++ src/components/FormGastos.vue
@@ -1,20 +1,20 @@
 <script>
 //Exporto el componente para que pueda ser utilizado en App.vue
 export default{
-    name: 'FormIngreso', //Nombre del componente
+    name: 'FormGastos', //Nombre del componente
 
     data() {
         return {
             //Defino las variables reactivas
             monto: '',       
-            categoria: ''     
+            categoria: '',    
         }
     },
 
     methods: {
     //Defino las funciones del componente
     
-        enviarIngreso() {
+        enviarGasto() {
             //Se ejecutará cuando el usuario envíe el formulario
             //Valido que no estén vacíos los campos
             if (!this.monto || !this.categoria){
@@ -22,13 +22,13 @@ export default{
             } 
 
             //Creo un objeto con los datos ingresados
-            const nuevoIngreso = {
+            const nuevoGasto = {
                 monto: Number(this.monto),  
                 categoria: this.categoria
             }
 
             // Avisamos al componente padre App.vue que hay un nuevo ingreso
-            this.$emit('agregar-ingreso', nuevoIngreso) //nombre del evento, objeto con datos
+            this.$emit('agregar-gasto', nuevoGasto) //nombre del evento, objeto con datos
 
             //limpio los datos del formulario
             this.monto = ''
@@ -40,7 +40,7 @@ export default{
 </script>
 
 <template>
-  <form @submit.prevent="enviarIngreso"> <!--La función que se ejecuta al enviar-->
+  <form @submit.prevent="enviarGasto"> <!--La función que se ejecuta al enviar-->
     <div>
       <label>Monto:</label>
       <input type="number" v-model="monto" placeholder="Ingrese el monto"> <!-- v-model="monto" se vincula el input con la variable del data. Cada vez que escribís algo, monto o categoria se actualiza automáticamente.-->
@@ -51,7 +51,7 @@ export default{
       <input type="text" v-model="categoria" placeholder="Ingrese la categoría">
     </div>
 
-    <button type="submit">Agregar ingreso</button>
+    <button type="submit">Agregar gasto</button>
   </form>
 </template>
 
@@ -72,7 +72,7 @@ input {
 
 button {
   padding: 8px;
-  background-color:#00162e;
+  background-color: #00162e;
   color: white;
   border: none;
   border-radius: 4px;
