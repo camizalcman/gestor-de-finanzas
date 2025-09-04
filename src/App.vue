4b661c826364da9f7972b65ed0332631e2e43138
camizalcman <camizalcman@gmail.com>
2025-09-03 23:17:13 -0300

    Crear base de form nuevos ingresos

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,42 @@
-<script setup>
-  const number = 5
+<script>
+  import Ingreso from './components/FormIngresos.vue'
 
-  function sum(a,b){
-    return a+b;
+  export default {
+      name: 'App',
+      components: {
+        Ingreso //Lo registramos para poder usarlo en el template
+    },
+
+    data() {
+      return {
+        ingresos: [] //Para guardar todos los ingresos
+      }
+    },
+
+    methods: {
+      agregarIngreso(nuevoIngreso) {
+        // Agregamos el ingreso que viene del hijo al array de ingresos
+        this.ingresos.push(nuevoIngreso)
+      }
+    }
   }
+ 
 </script>
 
 <template>
-  {{ sum(5,6) }}
+  <div>
+    <h1>Gestor de Ingresos</h1>
+    <Ingreso @agregar-ingreso="agregarIngreso"/> <!-- @agregar-ingreso escucha el evento que el hijo emite con $emit y agregarIngreso es el método del padre que se ejecuta (definido acá) cuando ocurre ese evento -->
+    <h2>Lista de ingresos:</h2>
+    <ul>
+      <li v-for="ingreso in ingresos"><!-- v-for recorre el array-->
+        {{ ingreso.monto }} - {{ ingreso.categoria }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
 
-  .text-red{
-    color:red
-  }
+
 </style>
---
+++ src/components/FormIngresos.vue
@@ -0,0 +1,81 @@
+<script>
+//Exporto el componente para que pueda ser utilizado en App.vue
+export default{
+    name: 'FormIngreso', //Nombre del componente
+
+    data() {
+        return {
+            //Defino las variables reactivas
+            monto: '',       
+            categoria: ''     
+        }
+    },
+
+    methods: {
+    //Defino las funciones del componente
+    
+        enviarIngreso() {
+            //Se ejecutará cuando el usuario envíe el formulario
+            //Valido que no estén vacíos los campos
+            if (!this.monto || !this.categoria){
+                return
+            } 
+
+            //Creo un objeto con los datos ingresados
+            const nuevoIngreso = {
+                monto: Number(this.monto),  
+                categoria: this.categoria
+            }
+
+            // Avisamos al componente padre App.vue que hay un nuevo ingreso
+            this.$emit('agregar-ingreso', nuevoIngreso) //nombre del evento, objeto con datos
+
+            //limpio los datos del formulario
+            this.monto = ''
+            this.categoria = ''
+        }
+    }
+
+}
+</script>
+
+<template>
+  <form @submit.prevent="enviarIngreso"> <!--La función que se ejecuta al enviar-->
+    <div>
+      <label>Monto:</label>
+      <input type="number" v-model="monto" placeholder="Ingrese el monto"> <!-- v-model="monto" se vincula el input con la variable del data. Cada vez que escribís algo, monto o categoria se actualiza automáticamente.-->
+    </div>
+
+    <div>
+      <label>Categoría:</label>
+      <input type="text" v-model="categoria" placeholder="Ingrese la categoría">
+    </div>
+
+    <button type="submit">Agregar ingreso</button>
+  </form>
+</template>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  max-width: 300px;
+  margin: 20px auto;
+}
+
+input {
+  padding: 6px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+button {
+  padding: 8px;
+  background-color: #007BFF;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+</style>
